0ebc9426a2f1445fb6fcf18e407039c32e6c1f1b
Rogério Ramos da Silva <habutre+github@gmail.com>
2019-05-23 08:35:30 +0200

    Adhere to credo advises

--- test/kafka_phoenix_lab/messaging/interaction_consumer.exs
+++ test/kafka_phoenix_lab/messaging/interaction_consumer.exs
@@ -4,10 +4,15 @@ defmodule KafkaPhoenixLab.Messaging.InteractionConsumer do
   alias KafkaEx.Protocol.Produce.Request
 
   test "interaction message schema" do
-    msg_payload = %Interaction{sent: 144, received: 7, timestamp: DateTime.from_iso8601("2019-05-01T10:38:47Z")}
+    msg_payload = %Interaction{
+      sent: 144,
+      received: 7,
+      timestamp: DateTime.from_iso8601("2019-05-01T10:38:47Z")
+    }
+
     message = %Message{key: "elixir-pub", value: msg_payload}
     request = %Request{topic: "interactions", partition: 0, required_acks: 1, messages: [message]}
 
-    assert request.messages.size == 1 
+    assert request.messages.size == 1
   end
 end
