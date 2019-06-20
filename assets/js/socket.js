d036a7cb8328e523f5131c8b6b742d606749a1e1
Rogerio Ramos da Silva <rogerio.ramosdasilva@takeaway.com>
2019-06-20 08:48:44 +0200

    Enable phoenix channels to broadcast kafka messages

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -55,7 +55,18 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("interactions:overview", {})
+let elixirDamage = document.querySelector("#elixir-damage")
+let scalaDamage = document.querySelector("#scala-damage")
+
+channel.on("elixir-damage", payload => {
+  elixirDamage.innerText = `${payload.damage}`
+})
+
+channel.on("scala-damage", payload => {
+  scalaDamage.innerText = `${payload.damage}`
+})
+
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
--- lib/kafka_phoenix_lab/messaging/interaction_consumer.ex
+++ lib/kafka_phoenix_lab/messaging/interaction_consumer.ex
@@ -6,9 +6,18 @@ defmodule KafkaPhoenixLab.Messaging.InteractionConsumer do
 
   def handle_message_set(message_set, state) do
     for message <- message_set do
+      publish_damage(message.key, message.value)
       Logger.debug("The message content will be sent to view:" <> inspect(message))
     end
 
     {:async_commit, state}
+  end
+
+  defp publish_damage("elixir-pub" = key, damage) do
+      KafkaPhoenixLabWeb.Endpoint.broadcast!("interactions:overview", "elixir-damage", %{damage: damage})
+  end
+
+  defp publish_damage("scala-pub" = key, damage) do
+      KafkaPhoenixLabWeb.Endpoint.broadcast!("interactions:overview", "scala-damage", %{damage: damage})
   end
 end
